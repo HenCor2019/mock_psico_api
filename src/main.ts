e463ff6c2af466de3d4b7c5acbe3bd483aff3a11
HenCor2019 <henry200amaya@gmail.com>
2022-05-27 18:12:18 -0600

    :sparkles: Add filters to users

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ import { config } from '@swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({ transform: true, skipMissingProperties: false }),
   );
